566438cbabe8c283ba0f5a94fd693b7d9ec82644
Htxe <ryancampos393@gmail.com>
2023-02-11 21:02:44 -0300

    first commit

---
+++ js/about.js
@@ -0,0 +1,10 @@
+function clicou() {
+    var n1 = Number(window.prompt('Digite um numero!'))
+    var n2 = Number(window.prompt('Digite outro Numero!'))
+    var s = n1 + n2
+
+    window.alert(`A soma dos numeros ${n1} e ${n2} é igual a ${s}`)
+    window.alert('Caso queira ver meus testes, vá na pagina principal e clique em "testes"')
+    
+
+}
