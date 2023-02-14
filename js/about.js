5cbd60ec2aea7f27423295d3fdb7a5bfde3e76aa
Htxe <ryancampos393@gmail.com>
2023-02-14 20:24:35 -0300

    v2

--- js/about.js
+++ js/about.js
@@ -1,10 +1,37 @@
 function clicou() {
-    var n1 = Number(window.prompt('Digite um numero!'))
+    var body = window.document.body.style.background = 'black'
+    var h1 = window.document.getElementById('h1text').style.color='white'
+    var p = window.document.getElementById ('ptext').style.color = 'white'
+    var a = window.document.getElementById('back').style.color = 'white'
+    var b = window.document.getElementById('back').style.background = 'black'
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*(var n1 = Number(window.prompt('Digite um numero!'))
     var n2 = Number(window.prompt('Digite outro Numero!'))
     var s = n1 + n2
 
     window.alert(`A soma dos numeros ${n1} e ${n2} é igual a ${s}`)
-    window.alert('Caso queira ver meus testes, vá na pagina principal e clique em "testes"')
+    window.alert('Caso queira ver meus testes, vá na pagina principal e clique em "testes"')*/
     
 
 }
